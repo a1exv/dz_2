ae488c871c3e6cda02a92487c481efa3f48ddbeb
a1exv <volodkevich.alex@gmail.com>
2017-03-19 13:06:20 +0300

    dz_2

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    partial class Auto
+    {
+        public override string ToString()
+        {
+            return (String.Format(name+" "+maxSpeed+" "+serialNo+" "+weight+" "+type));
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    partial class Auto
+    {
+        private string name;
+        private int maxSpeed;
+        private double weight;
+        private char type;
+        private string serialNo;
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int MaxSpeed { get; set; }
+        public double Weight { get; set; }
+        public char Type { get; set; }
+        public string SerialNo { get; set; }
+        public void GetSerialNumber(ref string snum)
+        {
+            snum = serialNo;
+        }
+        public static int count;
+        public static double totalWeight;
+        public Auto()
+        {
+            count++;
+            weight = 1000;
+            name = "default";
+            maxSpeed = 300;
+            type = 'A';
+            serialNo = "default";
+            totalWeight += weight;
+        }
+        public Auto(string name, int maxSpeed, double weight, char type, string serialNo)
+        {
+            this.name = name;
+            this.maxSpeed = maxSpeed;
+            this.weight = weight;
+            this.type = type;
+            this.serialNo = serialNo;
+            count++;
+            totalWeight += weight;
+        }
+         static Auto()
+        {
+            count = 0;
+            totalWeight = 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto[] arAuto = new Auto[5];
+
+            for (var i = 0; i < arAuto.Length; i++)
+            {
+                arAuto[i] = new Auto();
+                Console.WriteLine(arAuto[i].ToString());
+            }
+            Console.WriteLine(Auto.totalWeight);
+        }
+    }
+}
